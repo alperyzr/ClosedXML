b7a56e636d2e136b73054522282385a3a55b5a02
Alper YAZIR <alperyzrr@gmail.com>
2023-02-02 17:34:55 +0300

    update

--- ClosedXML.API/Program.cs
+++ ClosedXML.API/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddScoped<ICovidService, CovidService>();
 builder.Services.AddScoped<ICovidRepository, CovidRepository>();
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
